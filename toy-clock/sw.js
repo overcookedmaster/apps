50eea97c69866d599eb174ee4b97e67fd7bd7c95
overcookedmaster <suikai.wong@gmail.com>
2026-05-22 23:59:54 +0800

    Create sw.js

---
+++ toy-clock/sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'clock-teacher-v1';
+const URLS  = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => 
+  e.waitUntil( 
+    caches.open(CACHE).then(c => c.addAll(URLS))
+  )
+);
+
+self.addEventListener('activate', e => 
+  e.waitUntil( 
+    caches.keys().then(keys => Promise.all( 
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  )
+);
+
+self.addEventListener('fetch', e => 
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request))
+  )
+);
